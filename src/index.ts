691f58caf1f6b369c49bd5b18db852976834b5d9
crymfox <sameur.bh@gmail.com>
2023-10-17 16:52:23 +0100

    learning from mistakes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import express from "express"
-// import cors from "cors"
+import cors from "cors"
 import http from "http"
 import { Server } from "socket.io"
 
 const app = express()
 
-// app.use(cors({
-//   origin: "http://localhost:3000",
-//   credentials: true
-// }))
+app.use(cors)
 
 const server = http.createServer(app)
 const io = new Server(server, {
@@ -18,8 +15,37 @@ const io = new Server(server, {
   }
 })
 
+type userData = {
+  id: string
+  count: number
+}
+
+const users: userData[] = []
+
 io.on('connection', (socket) => {
   console.log(`a user connected: ${socket.id}`)
+  // users.push({ id: socket.id, count: 0 })
+  // add user to users array in the last position without using push
+  users.splice(users.length, 0, { id: socket.id, count: 2 })
+  console.log(users.length)
+  socket.on('disconnect', () => {
+    console.log(`user disconnected: ${socket.id}`)
+    const index = users.findIndex((user) => user.id === socket.id)
+    users.splice(index, 1)
+    // users.pop()
+    console.log(users.length)
+    socket.emit('users_updated', users)
+    socket.broadcast.emit('users_updated', users)
+  })
+  socket.on('count change', (count) => {
+    const index = users.findIndex((user) => user.id === socket.id)
+    users[index].count = count
+    console.log(users)
+    socket.emit('users_updated', users)
+    socket.broadcast.emit('users_updated', users)
+  })
+  socket.emit('users_updated', users)
+  socket.broadcast.emit('users_updated', users)
   // socket.on('disconnect', () => {
   //   console.log('user disconnected')
   // })
@@ -28,6 +54,6 @@ io.on('connection', (socket) => {
   // })
 })
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   console.log("Server started on port 5000")
 })
