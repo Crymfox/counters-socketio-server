f99ec4b5fe5415eafd336485a898734e681ba77b
crymfox <sameur.bh@gmail.com>
2023-10-31 14:22:25 +0000

    good

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,11 @@ const io = new Server(server, {
 })
 
 type userData = {
-  id: string
-  count: number
+  id: string,
+  count: number,
+  requests: string[],
+  admins: string[],
+  access: string[]
 }
 
 const users: userData[] = []
@@ -32,13 +35,20 @@ io.on('connection', (socket) => {
   console.log(`a user connected: ${socket.id}`)
   // users.push({ id: socket.id, count: 0 })
   // add user to users array in the last position without using push
-  users.splice(users.length, 0, { id: socket.id, count: 0 })
-  console.log(users.length)
+  const index = users.findIndex((user) => user.id === socket.id)
+  users.splice(users.length, 0, { id: socket.id, count: 0, requests: [], admins: [], access: [socket.id] })
+  console.log(users)
   update('users_updated')
 
   socket.on('disconnect', () => {
     console.log(`user disconnected: ${socket.id}`)
     const index = users.findIndex((user) => user.id === socket.id)
+    // remove the user from all the requests and admins arrays
+    users.forEach((user) => {
+      user.requests = user.requests.filter((request) => request !== index.toString())
+      user.admins = user.admins.filter((admin) => admin !== index.toString())
+      user.access = user.access.filter((access) => access !== index.toString())
+    })
     users.splice(index, 1)
     // users.pop()
     console.log(users.length)
@@ -50,6 +60,37 @@ io.on('connection', (socket) => {
     users[index].count = count
     // console.log(users)
     // socket.emit('count_changed', {count, index})
+    update('users_updated')
+  })
+
+  socket.on('request_access', ({socketId, key}) => {
+    const index = users.findIndex((user) => user.id === socketId)
+    if (!users[key].requests.includes(index.toString()) && !users[key].admins.includes(index.toString())) {
+      users[key].requests.push(index.toString())
+    }
+    update('users_updated')
+  })
+
+  socket.on('accept_request', ({socketId, key}) => {
+    console.log(socketId)
+    console.log(key)
+    const index = users.findIndex((user) => user.id === socketId)
+    users[index].admins.push(key)
+    users[index].requests = users[index].requests.filter((request) => request !== key)
+    users[index].access.push(users[key].id)
+    update('users_updated')
+  })
+
+  socket.on('reject_request', ({socketId, key}) => {
+    const index = users.findIndex((user) => user.id === socketId)
+    users[index].requests = users[index].requests.filter((request) => request !== key)
+    update('users_updated')
+  })
+
+  socket.on('remove_admin', ({socketId, key}) => {
+    const index = users.findIndex((user) => user.id === socketId)
+    users[index].admins = users[index].admins.filter((admin) => admin !== key)
+    users[index].access = users[index].access.filter((access) => access !== users[key].id)
     update('users_updated')
   })
   // socket.on('disconnect', () => {
